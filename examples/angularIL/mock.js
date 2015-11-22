785bca274186cd536f61c0671da97e3e8c2e93b4
Sagiv Frankel <sagivf@gmail.com>
2015-11-22 19:30:42 +0200

    add most states

---
+++ examples/angularIL/mock.js
@@ -0,0 +1,30 @@
+//angular.module('leonardo').run(['leoConfiguration', function (leoConfiguration) {
+//  leoConfiguration.addStates([
+//    {
+//      "name": "Characters",
+//      "url": "characters",
+//      "verb": "POST",
+//      "options": [
+//        {
+//          "name": "ok",
+//          "status": 200,
+//          "data": {},
+//          "delay": 0
+//        },
+//        {
+//          "name": "Failure",
+//          "status": "500",
+//          "data": {},
+//          "delay": 0
+//        }
+//      ],
+//      "active": true,
+//      "activeOption": {
+//        "name": "ok",
+//        "status": 200,
+//        "data": {},
+//        "delay": 0
+//      }
+//    }
+//  ]);
+//}]);
