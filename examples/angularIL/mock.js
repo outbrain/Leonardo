9d1ea308da2cf804c87725a3f2c28abeaa6a475c
tsachis <tsachishushan@gmail.com>
2015-12-08 17:50:21 +0200

    remove angular-mocks from the example application as well

--- examples/angularIL/mock.js
+++ examples/angularIL/mock.js
@@ -181,10 +181,10 @@ angular.module('leonardo').run(['leoConfiguration', '$rootScope', function (leoC
     ]
   });
 
-  var debug = leoConfiguration.getState('debug');
+  var debug = leoConfiguration.getActiveStateOption('debug');
   var isDebug = debug && debug.name === 'Enabled';
   $rootScope.$on('leonardo:setStates', function () {
-    var debug = leoConfiguration.getState('debug');
+    var debug = leoConfiguration.getActiveStateOption('debug');
     if (!isDebug && debug) {
       angular.reloadWithDebugInfo();
     }
