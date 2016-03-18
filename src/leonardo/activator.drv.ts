324844ad3062b3e547cc9a2d039d4ff85d237610
David Buchbut <dwidob@gmail.com>
2016-03-18 22:41:09 +0200

    initial typescript commit

--- src/leonardo/activator.drv.ts
+++ src/leonardo/activator.drv.ts
@@ -1,10 +1,17 @@
-angular.module('leonardo').directive('leoActivator', ['$compile', function activatorDirective($compile) {
+import ICompileService = angular.ICompileService;
+import IDirective = angular.IDirective;
+import IAugmentedJQuery = angular.IAugmentedJQuery;
+import IScope = angular.IScope;
+import IDocumentService = angular.IDocumentService;
+
+export function leoActivator($compile: ICompileService):IDirective {
+
   return {
     restrict: 'A',
     controllerAs: 'leonardo',
-    controller: LeoActivator,
+    controller: () => {},
     bindToController: true,
-    link: function(scope, elem) {
+    link: function(scope: IScope, elem: IAugmentedJQuery) {
       var el = angular.element('<div ng-click="leonardo.activate()" class="leonardo-activator" ng-show="leonardo.isLeonardoVisible"></div>');
       var win = angular.element([
       '<div class="leonardo-window">',
@@ -36,41 +43,5 @@ angular.module('leonardo').directive('leoActivator', ['$compile', function activ
       }, false );
     }
   };
-}]);
-
-LeoActivator.$inject = ['$scope', '$document'];
-function LeoActivator($scope, $document) {
-  this.isLeonardoVisible = true;
-  this.activeTab = 'scenarios';
-
-  this.selectTab = function (name) {
-    this.activeTab = name;
-  };
-
-  $document.on('keypress', function(e) {
-
-    if (e.shiftKey && e.ctrlKey) {
-      switch (e.keyCode) {
-        case 12:
-          this.isLeonardoVisible = !this.isLeonardoVisible;
-          break;
-        case 11:
-          this.activate();
-          break;
-        default:
-          break;
-      }
-      $scope.$apply();
-    }
-  }.bind(this));
-
-  this.activate = function() {
-    if (!document.body.classList.contains('pull-top')) {
-      document.body.classList.add('pull-top');
-      document.body.classList.remove('pull-top-closed');
-    }
-    else {
-      document.body.classList.remove('pull-top');
-    }
-  };
 }
+leoActivator.$inject = ['$compile'];
