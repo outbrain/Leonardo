276ad247cf382036271cec4da6d91c68429c0f33
MrFrankel <maorfrank@gmail.com>
2016-11-23 20:50:04 +0200

    Added recorder

--- src/leonardo/ui/views/recorder/recorder.ts
+++ src/leonardo/ui/views/recorder/recorder.ts
@@ -1,22 +1,23 @@
 /// <reference path="../../../leonardo.d.ts" />
 import Utils from '../../ui-utils';
 import Events from '../../ui-events';
+import RecorderList from './recorder-list/recorder-list';
 
 export default class Recorder {
+
+  viewNode: HTMLElement;
+  recorderList: RecorderList = new RecorderList();
 
   constructor() {
   }
 
   get() {
-    return Utils.getElementFromHtml(`<div id="leonardo-recorder" class="leonardo-recorder">Recorder</div>`);
-  }
-
-  getViewNode() {
-    return document.getElementById('leonardo-recorder');
+    return this.viewNode = Utils.getElementFromHtml(`<div id="leonardo-recorder" class="leonardo-recorder"</div>`);
   }
 
   render() {
-
+    this.viewNode.appendChild(this.recorderList.get());
+    this.recorderList.render();
   }
 
 }
