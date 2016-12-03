94efaf4bd1492d74bb80865026f5a307938063e3
Sagiv Frankel <sagivf@gmail.com>
2016-12-03 22:04:21 +0200

    start exported code

--- src/leonardo/ui/views/export/export.ts
+++ src/leonardo/ui/views/export/export.ts
@@ -4,19 +4,24 @@ import Events from '../../ui-events';
 
 export default class Export {
 
+  viewNode: HTMLElement;
+
   constructor() {
   }
 
   get() {
-    return Utils.getElementFromHtml(`<div id="leonardo-export" class="leonardo-export">Export</div>`);
-  }
-
-  getViewNode() {
-    return document.getElementById('leonardo-export');
+    return this.viewNode = Utils.getElementFromHtml(`<div id="leonardo-export" class="leonardo-export">
+      <button class="leonardo-button exportButtons" data-clipboard-target="#exportedCode"> Copy To Clipboard</button>
+      <button class="leonardo-button exportButtons" > Download Code</button>
+      <code contenteditable>
+        <div id="exportedCode">
+          
+        </div>
+      </code>
+    </div>`);
   }
 
   render() {
-
   }
 
 }
