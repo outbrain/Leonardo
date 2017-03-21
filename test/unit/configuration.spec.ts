dadeced0a916d4679bae509b5fbbccc57e7d0af9
David Buchbut <dwidob@gmail.com>
2017-03-21 23:05:58 +0200

    fix unit-test

--- test/unit/configuration.spec.ts
+++ test/unit/configuration.spec.ts
@@ -5,6 +5,33 @@ import { leoConfiguration } from '../../src/leonardo/configuration.srv'
 describe('addStates', function() {
 
   it('should add a state', function() {
-    expect(1).toBe(1);
+    var leo = leoConfiguration();
+
+    leo.addStates([
+      {
+        "name": "Create Character",
+        "url": "/character",
+        "verb": "POST",
+        "options": [
+          {
+            "name": "success",
+            "status": 200,
+            "data": {},
+            "delay": "2500"
+          },
+          {
+            "name": "Failure",
+            "status": "500",
+            "data": {
+              "msg": "you have a crappy server"
+            },
+            "delay": "2000"
+          }
+        ]
+      }
+    ]);
+
+    var states = leo.getStates();
+    expect(states.length).toBe(1);
   });
 });
