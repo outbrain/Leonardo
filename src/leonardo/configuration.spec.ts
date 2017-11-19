30740ebde50f7a7e68489ed66aed95d33f37d574
David Buchbut <dwidob@gmail.com>
2017-11-19 14:54:45 +0200

    add remove state unit test

--- src/leonardo/configuration.spec.ts
+++ src/leonardo/configuration.spec.ts
@@ -2,36 +2,45 @@
 /// <reference path="../../typings/jasmine/jasmine.d.ts" />
 import { leoConfiguration } from '../../src/leonardo/configuration.srv'
 
-describe('addStates', function() {
+const genericState = {
+  "name": "Create Character",
+  "url": "/character",
+  "verb": "POST",
+  "options": [
+    {
+      "name": "success",
+      "status": 200,
+      "data": {},
+      "delay": "2500"
+    },
+    {
+      "name": "Failure",
+      "status": "500",
+      "data": {
+        "msg": "you have a crappy server"
+      },
+      "delay": "2000"
+    }
+  ]
+};
 
+describe('addStates', function() {
   it('should add a state', function() {
     var leo = leoConfiguration();
 
-    leo.addStates([
-      {
-        "name": "Create Character",
-        "url": "/character",
-        "verb": "POST",
-        "options": [
-          {
-            "name": "success",
-            "status": 200,
-            "data": {},
-            "delay": "2500"
-          },
-          {
-            "name": "Failure",
-            "status": "500",
-            "data": {
-              "msg": "you have a crappy server"
-            },
-            "delay": "2000"
-          }
-        ]
-      }
-    ]);
+    leo.addStates([genericState]);
 
     var states = leo.getStates();
     expect(states.length).toBe(1);
+  });
+
+  it('should remove a state', function() {
+    var leo = leoConfiguration();
+
+    leo.addStates([genericState]);
+    leo.removeState(genericState);
+
+    var states = leo.getStates();
+    expect(states.length).toBe(0);
   });
 });
