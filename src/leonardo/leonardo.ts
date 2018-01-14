19c233f38d222f6d533d2c1d1950f95c26b0b216
MrFrankel <maorfrank@outbrain.com>
2018-01-14 17:16:24 +0200

    may the vim be with you

--- src/leonardo/leonardo.ts
+++ src/leonardo/leonardo.ts
@@ -1,5 +1,66 @@
+import {leoConfiguration} from './configuration.srv';
+import {Storage} from './storage.srv';
+import {polifylls} from './polyfills';
+import {Sinon} from './sinon.srv';
+import './style/app.less';
+import '../../node_modules/ace-builds/src/ace.js';
 
+declare const window;
+declare const Object;
 
+polifylls();
+
+//Init Configuration
+window.Leonardo = window.Leonardo || {};
+const configuration = leoConfiguration();
+const storage = new Storage();
+Object.assign(window.Leonardo || {}, configuration, {storage});
+Leonardo.loadSavedStates();
+
+// Init Sinon
+new Sinon();
+
+const launcher: any = document.createElement('div');
+let f: any;
+launcher.classList.add('leonardo-launcher');
+
+function toggleView() {
+  if (f.style.display === 'none') {
+    f.style.display = 'block';
+    f.contentDocument.getElementById('app').dispatchEvent(new Event('ui-show'));
+    document.body.classList.add('leonardo-launcher-active');
+  } else {
+    f.style.display = 'none';
+    document.body.classList.remove('leonardo-launcher-active');
+  }
+}
+
+function toggleLauncher() {
+  if (launcher.style.display === 'none') {
+    launcher.style.display = 'block';
+  } else {
+    launcher.style.display = 'none';
+  }
+}
+
+document.addEventListener('keyup', (e) => {
+  if (e.ctrlKey && e.shiftKey && e.keyCode === 76) {
+    return toggleLauncher();
+  }
+  if (f && e.ctrlKey && e.shiftKey && e.keyCode === 86) {
+    return toggleView();
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode === 67) {
+    return Leonardo.toggleConsoleOutput(Leonardo.get);
+  }
+  if (f && e.keyCode === 27 && f.style.display === 'block') {
+    return toggleView();
+  }
+
+});
+launcher.addEventListener('click', (e) => {
+  f && toggleView();
+  e.stopPropagation();
 });
 
 window.document.body.appendChild(launcher);
