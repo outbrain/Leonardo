9090be86398b5d846f4bfd0d3204f381982dcc82
David Buchbut <dwidob@gmail.com>
2018-01-14 16:52:41 +0200

    correct loading of iframe

--- src/leonardo/leonardo.ts
+++ src/leonardo/leonardo.ts
@@ -1,73 +1,12 @@
-import {leoConfiguration} from './configuration.srv';
-import {Storage} from './storage.srv';
-import {polifylls} from './polyfills';
-import {Sinon} from './sinon.srv';
-import './style/app.less';
-import '../../node_modules/ace-builds/src/ace.js';
 
-declare const window;
-declare const Object;
 
-polifylls();
-
-//Init Configuration
-window.Leonardo = window.Leonardo || {};
-const configuration = leoConfiguration();
-const storage = new Storage();
-Object.assign(window.Leonardo || {}, configuration, {storage});
-Leonardo.loadSavedStates();
-
-// Init Sinon
-new Sinon();
-
-const launcher: any = document.createElement('div');
-let f: any;
-launcher.classList.add('leonardo-launcher');
-
-function toggleView() {
-  if (f.style.display === 'none') {
-    f.style.display = 'block';
-    f.contentDocument.getElementById('app').dispatchEvent(new Event('ui-show'));
-    document.body.classList.add('leonardo-launcher-active');
-  } else {
-    f.style.display = 'none';
-    document.body.classList.remove('leonardo-launcher-active');
-  }
-}
-
-function toggleLauncher() {
-  if (launcher.style.display === 'none') {
-    launcher.style.display = 'block';
-  } else {
-    launcher.style.display = 'none';
-  }
-}
-
-document.addEventListener('keyup', (e) => {
-  if (e.ctrlKey && e.shiftKey && e.keyCode === 76) {
-    return toggleLauncher();
-  }
-  if (f && e.ctrlKey && e.shiftKey && e.keyCode === 86) {
-    return toggleView();
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode === 67) {
-    return Leonardo.toggleConsoleOutput(Leonardo.get);
-  }
-  if (f && e.keyCode === 27 && f.style.display === 'block') {
-    return toggleView();
-  }
-
-});
-launcher.addEventListener('click', (e) => {
-  f && toggleView();
-  e.stopPropagation();
 });
 
 window.document.body.appendChild(launcher);
 let timeout;
 function checkIframeLoaded() {
   let iframeDoc = f.contentDocument || f.contentWindow ?  f.contentWindow.document : {};
-  if (iframeDoc.readyState  == 'complete' && document.readyState  == 'complete' ) {
+  if (  iframeDoc.readyState  == 'complete' ) {
     clearTimeout(timeout);
     iframeDoc.write('<html><body></body></html>');
     iframeDoc.body.innerHTML = '<div id="app"></div>';
@@ -100,6 +39,6 @@ if (!window.Leonardo.storage.getNoUI()) {
     zIndex: 2147483646,
   });
 
-  document.body.appendChild(f);
   checkIframeLoaded();
+  document.body.appendChild(f);
 }
