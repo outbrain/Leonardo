b949787a8d1197f4404df83cc75c79f1a23f217d
sshabtay <sshabtay@outbrain.com>
2021-10-31 12:07:25 +0200

    adding clearAll

--- src/leonardo/ui/components/recorder/RecorderActionBar/RecorderActionBar.tsx
+++ src/leonardo/ui/components/recorder/RecorderActionBar/RecorderActionBar.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import './RecorderActionBar.less';
 
-export function RecorderActionBar({setFilter}) {
+export function RecorderActionBar({setFilter, clearAll}) {
 
   return (
     <div className="recorder-action-bar">
       <div className="recorder-search-container">
         <input placeholder="Search..." onKeyUp={(e: any) => setFilter(e.target.value)}/>
+      </div>
+      <div className="recorder-clear-container">
+          <button className="btn" onClick={() => clearAll()}>Clear All</button>
       </div>
     </div>
   );
