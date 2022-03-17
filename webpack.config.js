83c7cd585362904baff38ba9099c727c06329786
Omri Bar-Zik <obar-zik@outbrain.com>
2022-03-17 10:30:51 +0200

    tmp

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
-  }, 
+  },
 
   module: {
     rules: [
